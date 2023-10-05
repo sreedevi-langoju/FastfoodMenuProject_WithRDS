410b2305cb1d7be491cd6e6c04961fada78bae03
Sreedevi <dev.slangoju@gmail.com>
2023-10-05 11:12:49 -0500

    FastFood Project With RDS connection

---
+++ app.py
@@ -0,0 +1,68 @@
+from flask import Flask, render_template, request, redirect, url_for
+import databaselogic
+import datetime
+
+app = Flask(__name__)
+
+menu = databaselogic.get_menu_from_database()
+order = {}
+#order_num = 0  # Define order_num as a global variable and initialize it to 0
+
+@app.route('/', methods=['GET', 'POST'])
+def place_order():
+    global order_num  # Use a global variable for order_num
+    if request.method == 'POST':
+        order_date = datetime.datetime.now()  # Get the current date and time
+        for item_num, quantity in request.form.items():
+            if item_num.startswith('quantity_'):
+                item_num = item_num.replace('quantity_', '')
+                try:
+                    quantity = int(quantity)
+                    if quantity <= 0:
+                        continue
+                    
+                    if item_num in menu:
+                        if item_num in order:
+                            order[item_num]['quantity'] += quantity
+                        else:
+                            order[item_num] = {'name': menu[item_num]['name'], 'price': menu[item_num]['price'], 'quantity': quantity}
+                        
+                       
+                except ValueError:
+                    print("Invalid Quantity Value")
+
+        total_cost = round(calculate_total(order), 2)
+        #order_num += 1  # Increment order_num for each new order
+        # Insert the order into the MariaDB database tables
+        insert_data(order_date, total_cost, menu, request.form)
+        return render_template('receipt.html', order=order, total_cost=total_cost)
+    
+    return render_template('index.html', menu=menu)
+
+def calculate_total(order):
+    total = 0
+    for item_info in order.values():
+        total += item_info['price'] * item_info['quantity']
+    return total
+
+def insert_data(order_date, total_cost, menu, form_data):
+    # Insert the order into the "orders" table and retrieve the generated order_num
+    order_num = databaselogic.insert_order(order_date, total_cost)
+
+    for item_key, quantity in form_data.items():
+        if item_key.startswith('quantity_'):
+            item_num = item_key.replace('quantity_', '')  # Extract the item number from the key
+            try:
+                quantity = int(quantity)
+                if quantity <= 0:
+                    continue
+
+                if item_num in menu:
+                    # Use the same order_num in the "Orders_details" table
+                    databaselogic.insert_orders_details(menu[item_num]['name'], quantity, menu[item_num]['price'], order_date, total_cost, order_num)
+            except ValueError:
+                print("Invalid Quantity Value")
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
---
+++ databaselogic.py
@@ -0,0 +1,80 @@
+import mysql.connector
+
+#Establish a database connection
+def connectdatabase():
+    # Database configuration
+    db_config = {
+    'host': 'database-1.cojfedivjd6f.us-west-2.rds.amazonaws.com',
+    'user': 'admin',
+    'password': 'admin2801',
+    'database': 'fastfood'
+    }
+
+    # database connection
+    connection = mysql.connector.connect(**db_config)
+    return connection
+
+def openCursor(connection):
+    cursor = connection.cursor()
+    return cursor
+
+def closeAll(cursor,connection):
+    cursor.close()
+    connection.close()
+  
+
+def get_menu_from_database():
+    try:
+        connection=connectdatabase()
+        cursor=openCursor(connection)
+        # Execute the SQL query to fetch menu data
+        query = "SELECT Item_Num, Item_Name, Price, Image FROM Menu"
+        cursor.execute(query)
+
+        # Fetch all the rows as a list of tuples
+        menu_data = cursor.fetchall()
+
+        # Process the menu data and create the menu dictionary
+        menu = {}
+        for row in menu_data:
+            Item_Num, name, price, image = row
+            menu[str(Item_Num)] = {'name': name, 'price': float(price), 'image': image}
+
+        return menu
+
+    except mysql.connector.Error as err:
+        print(f"Database Error: {err}")
+    finally:
+        closeAll(cursor,connection)
+
+
+def insert_order(order_date,total_cost):
+    connection=connectdatabase()
+    cursor=openCursor(connection)
+    # Use prepared statements to insert the order into the database with the order date
+    insert_orders_query = "INSERT INTO Orders (Order_Date, Total_Cost) VALUES (%s,%s)"
+    orders_values = (order_date,total_cost)
+    try:
+        cursor.execute(insert_orders_query, orders_values)
+        connection.commit()
+        # Retrieve the generated order_num
+        order_num = cursor.lastrowid
+        return order_num
+    except mysql.connector.Error as err:
+        print(f"Database Error: {err}")
+    finally:
+        closeAll(cursor,connection)
+
+def insert_orders_details(item_name, quantity, price, order_date,total_cost,order_num):
+    connection=connectdatabase()
+    cursor=openCursor(connection)
+    # Use prepared statements to insert the order into the database with the order date
+    insert_orders_details_query = "INSERT INTO Orders_Details (Order_Num,Order_Date,Item_Name,Price, Quantity) VALUES (%s, %s, %s, %s, %s)"
+    orders_details_values = (order_num,order_date,item_name,price,quantity)
+    try:
+        cursor.execute(insert_orders_details_query,orders_details_values)
+        connection.commit()
+    except mysql.connector.Error as err:
+        print(f"Database Error: {err}")
+    finally:
+        closeAll(cursor,connection)
