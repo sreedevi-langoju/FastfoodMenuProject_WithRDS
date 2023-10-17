58cf9a858a1e8de5762fe8e9b3c0ab11894475c8
sreedevi-langoju <135724041+sreedevi-langoju@users.noreply.github.com>
2023-10-17 11:07:26 -0500

    removed the execute_query function

--- databaselogic.py
+++ databaselogic.py
@@ -15,42 +15,34 @@ def initialize_connection_and_cursor():
 
     try:
         connection = connect_database()
-        cursor = connection.cursor()
+        cursor = openCursor(connection)
     except mysql.connector.Error as err:
         print(f"Database Error: {err}")
     
     return connection, cursor
 
-def execute_query(connection, query, values=None):
-    cursor = openCursor(connection)
-    try:
-        if values is None:
-            cursor.execute(query)
-        else:
-            cursor.execute(query, values)
-        connection.commit()
-    except mysql.connector.Error as err:
-        print(f"Database Error: {err}")
-    finally:
-        closeAll(cursor, connection)
-
 def get_menu_from_database():
     connection, cursor = initialize_connection_and_cursor()
-    print("connection successfull in database logic")
+    print("Connection successful in database logic")
+    
     try:
         query = "SELECT Item_Num, Item_Name, Price, Image FROM Menu"
-        cursor = execute_query(connection, query)
+        cursor.execute(query)  # Execute the query
+        
         menu_data = cursor.fetchall()
-        print("fetching the menu_data in databaselogic ")
+        print("Fetching the menu_data in database logic")
+        
         menu = {}
         for row in menu_data:
             Item_Num, name, price, image = row
             menu[str(Item_Num)] = {'name': name, 'price': float(price), 'image': image}
-            print("Menu data name in for loop in database logic is ",name)
+            print("Menu data name in the for loop in database logic is", name)
         return menu
         
     except mysql.connector.Error as err:
         print(f"Database Error: {err}")
+    finally:
+        closeAll(cursor, connection)
 
 def insert_order(order_date, total_cost):
     connection, cursor = initialize_connection_and_cursor()
@@ -58,8 +50,9 @@ def insert_order(order_date, total_cost):
     try:
         query = "INSERT INTO Orders (Order_Date, Total_Cost) VALUES (%s, %s)"
         values = (order_date, total_cost)
-        cursor = execute_query(connection, query, values)
+        cursor.execute(query, values)
         order_num = cursor.lastrowid
+        connection.commit()
         return order_num
 
     except mysql.connector.Error as err:
@@ -71,7 +64,8 @@ def insert_orders_details(item_name, quantity, price, order_date, total_cost, or
     try:
         query = "INSERT INTO Orders_Details (Order_Num, Order_Date, Item_Name, Price, Quantity) VALUES (%s, %s, %s, %s, %s)"
         values = (order_num, order_date, item_name, price, quantity)
-        cursor = execute_query(connection, query, values)
+        cursor.execute(query, values)
+        connection.commit()
 
     except mysql.connector.Error as err:
         print(f"Database Error: {err}")
